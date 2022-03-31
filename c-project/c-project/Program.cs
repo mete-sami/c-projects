0af1f0545c5425b79b0ab0002eeb7826f66c6e68
mete-sami <98276995+mete-sami@users.noreply.github.com>
2022-04-01 01:00:13 +0300

    first commit

---
+++ c-project/c-project/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello World");
+        }
+    }
+}
